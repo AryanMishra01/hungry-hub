33c03bc38382790c1c76349c1ed8d7d4ea9c713f
AryanMishra01 <aryanmishrareact@gmail.com>
2024-05-27 05:07:17 -0700

    Header

--- App.js
+++ App.js
@@ -1,1 +1,35 @@
-<h1>Test</h1>
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container"> 
+                <img className="logo" src="https://i.postimg.cc/VLQY49Lz/Firefly-Hungry-Hub-Food-Delivery-App-Logo-Description-Create-a-modern-and-classy-logo-for-a-food.jpg"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+return (
+    <div className="app">
+        <Header/>
+    </div>
+)
+
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout/>);
---
+++ README.md
@@ -0,0 +1,13 @@
+/**
+ * Header
+ *  -Logo
+    -Nav Items
+ * Body
+    - Search
+    - Restro Container
+      - Restro Cards
+ * Footer
+     -  Copyright
+     -  Links
+     -  Address 
+ */
