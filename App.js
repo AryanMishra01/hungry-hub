cfda296a598599248a6e7569c7729be1b72ad259
AryanMishra01 <aryanmishrareact@gmail.com>
2024-05-29 09:56:41 -0700

    Dynamic Card

--- App.js
+++ App.js
@@ -24,38 +24,120 @@ const styleCard = {
    backgroundColor: "#f0f0f0"
 
 }
-const RestroCard = () => {
+const RestroCard = (props) => {
+const {resData} = props;
+
     return (
         <div className="restro-card" style={styleCard}>
-            <img className="restro-logo" alt="restro-logo" src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg"/>
-            <h3>Aryan Restaurant</h3>
-            <h4>Fast Food, Chinese, Continental</h4>
-            <h4>4.5 stars</h4>
-            <h4>30 minutes</h4>
+            <img className="restro-logo" alt="restro-logo" src= {"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resData.info.cloudinaryImageId} />
+            <h3>{resData.info.name}</h3>
+            <h4>{resData.info.cuisines.join(", ")}</h4>
+            <h4>{resData.info.avgRating} stars</h4>
+            <h4>{resData.info.costForTwo}</h4>
+            <h4>{resData.info.sla.deliveryTime} minutes</h4>
         </div>
     );
 };
 
+const restroObj = {
+    info: {
+        id: "55473",
+        name: "Pizza Hut",
+        cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+        locality: "Sector 10",
+        areaName: "Rohini",
+        costForTwo: "₹350 for two",
+        cuisines: [
+          "Pizzas",
+          "Garlic Bread",
+          "Choco Volcano"
+        ],
+        avgRating: 4,
+        parentId: "721",
+        avgRatingString: "4.0",
+        totalRatingsString: "5K+",
+        sla: {
+          deliveryTime: 34,
+          lastMileTravel: 2.3,
+          serviceability: "SERVICEABLE",
+          slaString: "30-35 mins",
+          lastMileTravelString: "2.3 km",
+          iconType: "ICON_TYPE_EMPTY"
+        },
+        availability: {
+          nextCloseTime: "2024-05-30 04:00:00",
+          opened: true
+        },
+        badges: {
+          imageBadges: [
+            {
+              "imageId": "Rxawards/_CATEGORY-Pizza.png",
+              "description": "Delivery!"
+            }
+          ]
+        },
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {
+              badgeObject: [
+                {
+                  attributes: {
+                    description: "Delivery!",
+                    imageId: "Rxawards/_CATEGORY-Pizza.png"
+                  }
+                }
+              ]
+            },
+            textBased: {
+              
+            },
+            textExtendedBadges: {
+              
+            }
+          }
+        },
+        aggregatedDiscountInfoV3: {
+          header: "ITEMS",
+          subHeader: "AT ₹189"
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {
+              
+            },
+            video: {
+              
+            }
+          }
+        },
+        reviewsSummary: {
+          
+        },
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {
+          
+        }
+      },
+      analytics: {
+        
+      },
+      cta: {
+        "link": "https://www.swiggy.com/restaurants/pizza-hut-sector-10-rohini-delhi-55473",
+        "type": "WEBLINK"
+      },    
+}
+    
 
 const Body = () => {
     return (
        <div className="body">
          <div className="search">Search</div>
          <div className="restro-container">
-             <RestroCard/>
-             <RestroCard/>
-             <RestroCard/>
-             <RestroCard/>
-             <RestroCard/>
-             <RestroCard/>
-             <RestroCard/>
-             <RestroCard/>
-             <RestroCard/>
-             <RestroCard/>
-             <RestroCard/>
-             <RestroCard/>
-             <RestroCard/>
-             <RestroCard/>
+             <RestroCard resData ={restroObj} />
          </div>
        </div>
     )  
--- README.md
+++ README.md
@@ -40,4 +40,77 @@
             </div>
              )
             }
+    Making restro cards dynamic     
+- props
+     -  used to pass properties to the component
+     - these are just like passing normal arguments to a function.
+     - when you have to dynamic pass data to a component we pass it as a prop
+      
+         e.g:  
+         const RestroCard = (props) => {
+    return (
+        <div className="restro-card" style={styleCard}>
+            <img className="restro-logo" alt="restro-logo" src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg"/>
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>4.5 stars</h4>
+            <h4>30 minutes</h4>
+        </div>
+    );
+};     
          
+       const Body = () => {
+    return (
+       <div className="body">
+         <div className="search">Search</div>
+         <div className="restro-container">
+             <RestroCard resName="Aryan Restaurant" cuisine="Contiental, Chinese, Fast Food & more"/>
+             <RestroCard resName="McDonalds" cuisine="Burger, Fries & more"/>
+             
+         </div>
+       </div>
+    )  
+}
+
+OR
+------------
+const RestroCard = (props) => {
+const {resName, cuisine} = props
+
+    return (
+        <div className="restro-card" style={styleCard}>
+            <img className="restro-logo" alt="restro-logo" src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg"/>
+            <h3>{resName}</h3>
+            <h4>{cuisine}</h4>
+            <h4>4.5 stars</h4>
+            <h4>30 minutes</h4>
+        </div>
+    );
+};
+----------------
+We can write the above code as below also:
+-Destructing on the fly:
+      - this is in JavaScript
+      - 
+         const RestroCard = ({resName,cuisine}) => {
+    return (
+        <div className="restro-card" style={styleCard}>
+            <img className="restro-logo" alt="restro-logo" src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg"/>
+            <h3>{resName}</h3>
+            <h4>{cuisine}</h4>
+            <h4>4.5 stars</h4>
+            <h4>30 minutes</h4>
+        </div>
+    );
+};     
+         
+-----
+# JSON VIEWER extension download.
+--
+# Config Driven UI:
+    * The UI changes based on the data feeded from the backend.
+    e.g  UI's will be different as per data provided to the cities.
+
+# To separate array with comma's use:
+     join(", ") 
+        e.g.   <h4>{resData.info.cuisines.join(", ")}</h4>   
