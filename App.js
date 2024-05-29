f7db65443953c6ef05c5cdfa8ea675e44351b230
AryanMishra01 <aryanmishrareact@gmail.com>
2024-05-29 01:57:08 -0700

    Static Restro Cards

--- App.js
+++ App.js
@@ -20,10 +20,54 @@ const Header = () => {
     )
 }
 
+const styleCard = {
+   backgroundColor: "#f0f0f0"
+
+}
+const RestroCard = () => {
+    return (
+        <div className="restro-card" style={styleCard}>
+            <img className="restro-logo" alt="restro-logo" src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg"/>
+            <h3>Aryan Restaurant</h3>
+            <h4>Fast Food, Chinese, Continental</h4>
+            <h4>4.5 stars</h4>
+            <h4>30 minutes</h4>
+        </div>
+    );
+};
+
+
+const Body = () => {
+    return (
+       <div className="body">
+         <div className="search">Search</div>
+         <div className="restro-container">
+             <RestroCard/>
+             <RestroCard/>
+             <RestroCard/>
+             <RestroCard/>
+             <RestroCard/>
+             <RestroCard/>
+             <RestroCard/>
+             <RestroCard/>
+             <RestroCard/>
+             <RestroCard/>
+             <RestroCard/>
+             <RestroCard/>
+             <RestroCard/>
+             <RestroCard/>
+         </div>
+       </div>
+    )  
+}
+
+
+
 const AppLayout = () => {
 return (
     <div className="app">
         <Header/>
+        <Body />
     </div>
 )
 
--- README.md
+++ README.md
@@ -6,8 +6,38 @@
     - Search
     - Restro Container
       - Restro Cards
+         - Image
+         - Name of Resturant, Star Rating, cuisine, dilevery time.
  * Footer
      -  Copyright
      -  Links
      -  Address 
  */
+
+ CSS:
+   - Inside index.css
+   - Inline style in index.html
+          e.g: <div id="root" class="root" style="width 20px"></div>
+   - Directly by using a javascript object:
+            e.g: Here styleCard is JS object
+          const styleCard = {
+           backgroundColor: "grey"
+          }
+          const RestroCard = () => {
+           return (
+            <div className="restro-card" style={StyleCard}>
+            <h3>Aryan Resturant</h3>
+            </div>
+             )
+            }
+
+            or we can directly ingest it by below syntax"
+               e.g:
+           const RestroCard = () => {
+           return (
+            <div className="restro-card" style={{backgroundColor: "grey"}}>
+            <h3>Aryan Resturant</h3>
+            </div>
+             )
+            }
+         
